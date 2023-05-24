f6af28babff486906729a3dba524b0c0ef270684
Anni <anni.siren@gmail.com>
2023-05-24 17:34:08 -0400

    Added dataset updating functionality - remove empty lines and update accordingly (works, including cells with multiple numbers)

--- func_update_dataset.py
+++ func_update_dataset.py
@@ -1,12 +1,12 @@
 from csv import DictReader
 import pandas as pd
-
+import numpy as np
 
 def open_file(filepath):
     df = pd.read_csv(filepath)
     df.set_index('identifier', inplace=True)
-    return df
 
+    return df
 
 def save_file(df, filepath):
     file_location = filepath.split('.csv')[0]
@@ -14,36 +14,36 @@ def save_file(df, filepath):
 
     df.to_csv(save_file_location, encoding='utf-8')
 
-
 def remove_empty_lines(df):
     df = df.dropna(how='all')
     return df
-
 
 def shift_nodes_after_empty_lines(df):
     old_index = -1
     for index, row in df.iterrows():
         index_int = int(index)
         new_index = old_index + 1
+
         if index_int != new_index:
-            print("Index out of sync")
-
-
             df.rename(index={index: str(new_index)}, inplace=True)
             df = df.replace(index, str(new_index))
 
-            # print(index)
-            # print(old_index)
-            # print(row)
-            # input("Press enter to continue...")
+            df = check_alt_list(df, str(index), str(new_index))
 
         old_index = new_index
 
     return df
 
-# def move_nodes(df, shift, start):
-#
-#     for index, row in df.iterrows():
-#         if index == start:
-#
-#             print("")
+def check_alt_list(df, old_index, new_index):
+    for index, row in df.iterrows():
+        try:
+           if np.isnan(df.at[index, 'contains']):
+            pass
+        except:
+            if old_index in df.loc[index,'contains']:
+                # print(df.loc[index, 'contains'])
+                df.loc[index,'contains'] = df.loc[index, 'contains'].replace(old_index, new_index)
+                # print(df.loc[index,'contains'])
+                # print("---")
+
+    return df
--- update_dataset.py
+++ update_dataset.py
@@ -2,7 +2,7 @@ import os
 import func_update_dataset as helper
 import tkinter as tk
 from tkinter.filedialog import askopenfilename
-
+import pandas as pd
 
 # Datastructure
 # {ID1: { data for ID1 from CSV }, ID2: { data for ID2 from CSV } ... IDN: { data for IDN from CSV }}
@@ -22,16 +22,11 @@ def read_file():
 # def main():
 if __name__ == '__main__':
     filepath = read_file()
-    file_dict = helper.open_file(filepath)
+    file_df = helper.open_file(filepath)
 
-    file_dict = helper.remove_empty_lines(file_dict)
-    file_dict = helper.shift_nodes_after_empty_lines(file_dict)
-
-    print(file_dict)
-
-    # input("Press enter to continue...")
-
-    helper.save_file(file_dict,filepath)
+    file_df = helper.remove_empty_lines(file_df)
+    file_df = helper.shift_nodes_after_empty_lines(file_df)
+    helper.save_file(file_df,filepath)
 
     while True:
         print("The following actions are available: ")
