cadf7c1d61ea31b0ea7d78d6736dcd355f0fb20d
Anni <anni.siren@gmail.com>
2023-05-29 13:19:38 -0400

    Added dataset updating functionality - updated menu with: add row, delete row, delete empty rows, edit row, print current, save current - Added delete row - Added print dataset - TBD: add row, edit row

--- func_update_dataset.py
+++ func_update_dataset.py
@@ -18,13 +18,45 @@ def remove_empty_lines(df):
     df = df.dropna(how='all')
     return df
 
+# identifier, title, description, url, type, assesses, comesAfter, alternativeContent, requires, contains, isPartOf, isFormatOf
+def delete_row(df, id):
+    id = int(id)
+    df = delete_relationship(df, id)
+    df = df.drop(id)
+    df = shift_nodes_after_empty_lines(df)
+    return df
+
+def delete_relationship(df, id):
+    df = df.replace(id, np.nan)
+    df = delete_relationship_lists(df, id)
+
+    return df
+
+def delete_relationship_lists(df, id):
+    for index, row in df.iterrows():
+        try:
+            if np.isnan(df.at[index, 'contains']):
+                pass
+        except:
+            df.loc[index,'contains'] = df.loc[index, 'contains'].replace(str(id), np.nan)
+
+        try:
+            if np.isnan(df.at[index, 'requires']):
+                pass
+        except:
+            df.loc[index, 'requires'] = df.loc[index, 'requires'].replace(str(id), np.nan)
+
+    return df
+
 def shift_nodes_after_empty_lines(df):
     old_index = -1
+    change = False
     for index, row in df.iterrows():
         index_int = int(index)
         new_index = old_index + 1
 
         if index_int != new_index:
+            change = True
             df.rename(index={index: str(new_index)}, inplace=True)
             df = df.replace(index, str(new_index))
 
@@ -32,18 +64,17 @@ def shift_nodes_after_empty_lines(df):
 
         old_index = new_index
 
+    if change:
+        print("Changed index of node(s).")
+
     return df
 
 def check_alt_list(df, old_index, new_index):
     for index, row in df.iterrows():
         try:
-           if np.isnan(df.at[index, 'contains']):
-            pass
+            if np.isnan(df.at[index, 'contains']):
+                pass
         except:
             if old_index in df.loc[index,'contains']:
-                # print(df.loc[index, 'contains'])
                 df.loc[index,'contains'] = df.loc[index, 'contains'].replace(old_index, new_index)
-                # print(df.loc[index,'contains'])
-                # print("---")
-
     return df
--- update_dataset.py
+++ update_dataset.py
@@ -24,16 +24,41 @@ if __name__ == '__main__':
     filepath = read_file()
     file_df = helper.open_file(filepath)
 
-    file_df = helper.remove_empty_lines(file_df)
-    file_df = helper.shift_nodes_after_empty_lines(file_df)
-    helper.save_file(file_df,filepath)
+    # file_df = helper.remove_empty_lines(file_df)
+    # file_df = helper.shift_nodes_after_empty_lines(file_df)
+    # helper.save_file(file_df, filepath)
 
     while True:
         print("The following actions are available: ")
         print("Update (u). Quit (q).")
         choice = input('Input:  ')
         if choice.lower() == 'update' or choice.lower() == 'u':
-            print("")
+            print("Options: Add row (a), Delete row (d), Delete empty rows (del), "
+                 "Edit row (e), Print current (p), Save current (s)")
+            choice = input('Input:  ')
+            match choice:
+                case "a":
+                    print("Add row")
+                    print("TBD")
+
+                case "d":
+                    print("Delete row")
+                    to_delete = input('What row do you want to delete. Give Identifier:  ')
+                    file_df = helper.delete_row(file_df, to_delete)
+
+                case "del":
+                    print("Delete empty rows")
+                    file_df = helper.remove_empty_lines(file_df)
+                    file_df = helper.shift_nodes_after_empty_lines(file_df)
+
+                case "e":
+                    print("Edit row")
+                    print("TBD")
+
+                case "p":
+                    print(file_df.to_markdown())
+                case "s":
+                    helper.save_file(file_df, filepath)
 
         elif choice.lower() == 'quit' or choice.lower() == 'q':
             print("Program exit.")
